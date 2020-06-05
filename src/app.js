c7a022cf754d7335325f6e2b1601e834fca17367
punam-shelke <punamshelke.98@gmail.com>
2020-06-05 16:21:58 +0530

    FSJ0520A101

--- src/app.js
+++ src/app.js
@@ -23,11 +23,26 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
-
+    let player = {};
+  let  detailedPlayers = players.map(function (name ,index){
+        player = {};
+        player.name = name;
+        player.strength = getRandomStrength();
+        player.image =`images/super-${index+1}.png`;
+        if(index % 2){
+            player.type = `villain`;
+        }
+        else{
+            player.type = `hero`;
+        }
+        console.log(player);
+        
+        return player;
+        
+    });
     // Instead of forloop use Map method
     // Code here
-
+    console.log(detailedPlayers)
     return detailedPlayers;
 }
 
@@ -39,11 +54,37 @@ const getRandomStrength = () => {
 // Build player template
 const buildPlayers = (players, type) => {
     let fragment = '';
+    let div1 , pic ,div2 ,div3;
+    fragment = players.filter(function (player){
+      //  console.log(player);
+      //  console.log(player.type);
+        return player.type == type;
+    }).map(function(player){
 
+            div1 = document.createElement('div');
+            div1.className = 'player';
+            pic = document.createElement('img');
+            pic.src = `${player.image}`;
+           // console.log(pic.src);
+            pic.alt = ``;
+            div2 = document.createElement('div');
+            div2.className = 'name';
+            div2.textContent  = `${player.name}`;
+            div3 = document.createElement('div');
+            div3.className = 'strength';
+            div3.textContent  = `${player.strength}`;
+            div1.appendChild(pic);
+            div1.appendChild(div2);
+            div1.appendChild(div3);
+          console.log(div1.outerHTML);
+            return div1.outerHTML;
+    }).reduce(function (initial , Current){
+        return initial + Current;
+    },``)
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
+    
     return fragment;
 }
 
